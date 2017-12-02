8ae2ba3151c6bd117705b2b23e9ded5604b53276
Terry Schene <terrys283@yahoo.com>
2017-12-01 20:44:23 -0600

    one fix

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -9,7 +9,7 @@ namespace TechJobs.Controllers
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
-            //ViewBag.title = "Search";
+            ViewBag.title = "Search";
             return View();
         }
 
